3219bab8b3a76cbaf55a38dd9a2cfde0faec17b6
FlvBiel <106493001+FlvBiel@users.noreply.github.com>
2024-12-09 09:07:01 -0300

    Create PerfilFuncionario.java

---
+++ src/teste/PerfilFuncionario.java
@@ -0,0 +1,62 @@
+import java.util.Date;
+
+public class PerfilFuncionario {
+    private int numeroIdentificacao;
+    private String nome;
+    private String cargo;
+    private int salario;
+    private Date dataContratacao;
+    private Date dataDesligamento;
+    private StatusFuncionario status;
+
+    public PerfilFuncionario(int numeroIdentificacao, String nome, String cargo, int salario, Date dataContratacao) {
+        this.numeroIdentificacao = numeroIdentificacao;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.dataContratacao = dataContratacao;
+        this.status = StatusFuncionario.ATIVO;
+    }
+
+    public void criar() {
+        System.out.println("Funcionário criado: " + nome + ", Cargo: " + cargo);
+    }
+
+    public void atualizarStatus(StatusFuncionario novoStatus) {
+        this.status = novoStatus;
+        System.out.println("Status do funcionário " + nome + " atualizado para: " + status);
+    }
+
+    public void detalhar() {
+        System.out.println("Detalhes do Funcionário:");
+        System.out.println("- ID: " + numeroIdentificacao);
+        System.out.println("- Nome: " + nome);
+        System.out.println("- Cargo: " + cargo);
+        System.out.println("- Salário: " + salario);
+        System.out.println("- Data de Contratação: " + dataContratacao);
+        if (dataDesligamento != null) {
+            System.out.println("- Data de Desligamento: " + dataDesligamento);
+        }
+        System.out.println("- Status: " + status);
+    }
+
+    public void excluir() {
+        System.out.println("Funcionário excluído: " + nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public StatusFuncionario getStatus() {
+        return status;
+    }
+
+    public void setDataDesligamento(Date dataDesligamento) {
+        this.dataDesligamento = dataDesligamento;
+    }
+}
+
+enum StatusFuncionario {
+    ATIVO, APOSENTADO, DESLIGADO
+}
