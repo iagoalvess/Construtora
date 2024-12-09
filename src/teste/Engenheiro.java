bf59fc2618417c29467a26d58e60bcfb51224ce0
FlvBiel <106493001+FlvBiel@users.noreply.github.com>
2024-12-09 09:05:46 -0300

    Create Engenheiro.java

---
+++ src/teste/Engenheiro.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Engenheiro extends Usuario {
+    private List<Integer> projetosCoordenados = null;
+
+    public Engenheiro(int numeroIdentificacao, String nome, String cpf, String cargo, List<Integer> projetosCoordenados) {
+        super(numeroIdentificacao, nome, cpf, cargo);
+        this.projetosCoordenados = projetosCoordenados;
+    }
+
+    @Override
+    public void criarUsuario() {
+        System.out.println("Engenheiro " + nome + " foi criado.");
+    }
+
+    public List<Integer> getProjetosCoordenados() {
+
+        return projetosCoordenados;
+    }
+}
